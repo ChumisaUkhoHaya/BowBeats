ab5b184b1f1b2c0aaa8ece3f1136c18b63375adb
ChumisaUkhoHaya <chumisahaya0@gmail.com>
2024-11-09 19:55:54 +0200

    update

---
+++ src/assets/assets.js
@@ -0,0 +1,185 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import BowBeats_logo from './BowBeats_logo.png'
+import clock_icon from './clock_icon.png'
+import img from './img.jpeg'
+import img2 from './img2.jpeg'
+import img3 from './img3.jpeg'
+import img4 from './img4.jpeg'
+import img5 from './img5.jpeg'
+import img7 from './img7.png'
+import img8 from './img8.png'
+import img9 from './img9.png'
+import img10 from './img10.png'
+import img11 from './img11.png'
+import img12 from './img12.png'
+import img14 from './img14.jpeg'
+import img15 from './img15.png'
+import img16 from './img16.png'
+import SailorMoon from './SailorMoon.mp3'
+import Suited from  './Suited.mp3'
+import AgoraHills from  './AgoraHills.mp3'
+import HrsHrs from './HrsHrs.mp3'
+import NickiMinaj from './NickiMinaj.mp3'
+import TiaTameraftRicoNasty from './TiaTameraftRicoNasty.mp3'
+import Beyoncé from './Beyoncé.mp3'
+import LUV from './LUV.mp3'
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    BowBeats_logo,
+    clock_icon
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Your Top Trending Female Artists",
+        image: img8,
+        desc:"Celebrate Women with BowBeats.",
+        bgColor:"#d44476"
+    },
+    {   
+        id:1,
+        name: "Good Vibes",
+        image: img9,
+        desc:"Experience Good Vibes with BowBeats.",
+        bgColor:"#b7697a"
+    },
+    {   
+        id:2,
+        name: "Trending With BowBeats",
+        image: img10,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#e7edc0"
+    },
+    {   
+        id:3,
+        name: "Songs You never skip",
+        image: img16,
+        desc:"Your weekly update of you're most played tracks",
+        bgColor:"#f4e2ff"
+    },
+    {   
+        id:4,
+        name: "Mega Hits,",
+        image: img11,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#795da5"
+    },
+    {   
+        id:5,
+        name: "Bowie Favorites",
+        image: img15,
+        desc:"Your weekly update of the most played tracks",
+        bgColor:"#ede9eb"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Tia Tamera.ft Rico Nasty",
+        image: img,
+        file:TiaTameraftRicoNasty,
+        desc:"Doja Cat",
+        duration:"3:00"
+    },
+    {
+        id:1,
+        name: "Drunk In Love ft. JAY Z",
+        image: img7,
+        file:Beyoncé,
+        desc:"Beyoncé",
+        duration:"2:20"
+    },
+    {
+        id:2,
+        name: "Agora Hills",
+        image: img3,
+        file:AgoraHills,
+        desc:"Doja Cat",
+        duration:"2:32"
+    },
+    {
+        id:3,
+        name: "Hrs & Hrs",
+        image: img4,
+        file:HrsHrs,
+        desc:"Muni Long",
+        duration:"2:50"
+    },
+    {
+        id:4,
+        name: "Moment 4 life",
+        image: img5,
+        file:NickiMinaj,
+        desc:"Nicki Minaj",
+        duration:"3:10"
+    },
+    {
+        id:5,
+        name: "Sailor Moon",
+        image: img14    ,
+        file:SailorMoon,
+        desc:"LAYA",
+        duration:"2:45"
+    },
+    {
+        id:6,
+        name: "Suited ",
+        image: img2,
+        file:Suited,
+        desc:"Shekhina",
+        duration:"2:18"
+    },
+    {
+        id:7,
+        name: "LUV",
+        image: img12,
+        file:LUV,
+        desc:"Tory Lanez",
+        duration:"2:35"
+    }
+]
